8e42b980c040ad9bf77b79eb8b4e752b3a1d593c
Vikas <vikas.vallabhaneni@outlook.com>
2025-09-21 18:02:13 -0400

    updated model

--- lambda_function.py
+++ lambda_function.py
@@ -1,6 +1,6 @@
 # lambda_function.py
 import json
-from litellm import completion
+from litellm import completion # pyright: ignore[reportMissingImports]
 import time
 
 def lambda_handler(event, context):
@@ -12,7 +12,7 @@ def lambda_handler(event, context):
     models = [
         "gpt-3.5-turbo",
         "claude-3-haiku-20240307", 
-        "groq/llama3-8b-8192"
+        "groq/llama3-70b-8192"
     ]
     
     for model in models:
